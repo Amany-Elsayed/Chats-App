076d815b3f191a57a0e0470d1562b50617a07067
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-05-19 19:11:00 +0300

    fixed frontend not connecting issue

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const redirect = sessionStorage['redirect'];
+delete sessionStorage['redirect'];
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, '', redirect);
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
