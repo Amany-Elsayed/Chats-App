b48be92886130be6a116cc7008da54b277fcc9e1
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-05-19 18:56:29 +0300

    improved server connection

--- server/server.js
+++ server/server.js
@@ -30,7 +30,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 socket(server)
 
-server.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
     console.log(`server running on port ${process.env.PORT}`);
 })
 
